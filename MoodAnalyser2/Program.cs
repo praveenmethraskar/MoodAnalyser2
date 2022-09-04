7ddd6e4bc57f260c6b3de62e8396cb9dbe9f2ec6
praveenmethraskar <55995179+praveenmethraskar@users.noreply.github.com>
2022-09-04 13:36:17 +0530

    Update Program.cs

--- MoodAnalyser2/Program.cs
+++ MoodAnalyser2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace MoodAnalyser2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+
+            //Creating object of MoodAnalyser class
+
+            MoodAnalyser moodobj = new MoodAnalyser();
+
+            Console.WriteLine("Enter your Mood:");
+            string message = Console.ReadLine();
+
+            Console.WriteLine(moodobj.AnalyseMood(message));
+        }
+    }
+}
