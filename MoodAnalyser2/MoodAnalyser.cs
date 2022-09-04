30fc7516760257e6eaca799296a98e04e4bb4e15
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-04 13:30:36 +0530

    Returning happy or sad

---
+++ MoodAnalyser2/MoodAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser2
+{
+    public class MoodAnalyser
+    {
+        public string AnalyseMood(string message)
+        {
+            if (message.ToUpper().Contains("SAD"))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Happy Mood";
+            }
+        }
+    }
+}
