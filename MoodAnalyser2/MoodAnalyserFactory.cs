4b24275a6bdf7e020c4cfbe402170ebe948661cb
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-04 15:10:44 +0530

    Reflection

---
+++ MoodAnalyser2/MoodAnalyserFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser2
+{
+    public class MoodAnalyserFactory
+    {
+        public static object CreateMoodAnalyser(string className)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type moodAnalysetype = assembly.GetType(className);
+            return Activator.CreateInstance(moodAnalysetype);
+        }
+    }
+}
